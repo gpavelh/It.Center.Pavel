db0a13f035d1e85a9c376ec785336f8a27242d18
Pavel <gh7777@yandex.ru>
2020-02-12 15:29:11 +0300

    Ex2

---
+++ src/Ex2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        if (a < 0 && a % 2 == 0) {
+            System.out.println("Число " + a + " отрицательне и четное");
+        }
+        else if (a > 0 && a % 2 == 0) {
+            System.out.println("Число " + a + " положительное и четное");
+        }
+        else if (a < 0 && a % 2 != 0) {
+            System.out.println("Число " + a + " отрицательне и нечетное");
+        }
+        else if (a > 0 && a % 2 != 0) {
+            System.out.println("Число " + a + " положительное и нечетное");
+        }
+        else if (a == 0) {
+            System.out.println("Число " + a + " нулевое");
+        }
+
+    }
+}
