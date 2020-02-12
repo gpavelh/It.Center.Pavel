96bbe3a28686e9c48c033512157c098c0ee2cea2
Pavel <gh7777@yandex.ru>
2020-02-12 15:30:08 +0300

    Ex3

---
+++ src/Ex3.java
@@ -0,0 +1,13 @@
+public class Ex3 {
+    public static void main(String [] args){
+        for (int i = 1; i <=10; i++){
+            for(int y = 1; y <= 10; y++){
+                if (y == 10){
+                    System.out.println(i*y);
+                }
+                else
+                    System.out.print(i*y + " ");
+            }
+        }
+    }
+}
