187215575dc7f2d7e6de32b34a58badc9a4c5c73
Pavel <gh7777@yandex.ru>
2020-02-12 15:27:15 +0300

    Ex1

---
+++ src/Ex1.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine()), b = Integer.parseInt((reader.readLine()));
+        if (a > b){
+            System.out.println("Минимальное число " + b);
+        }
+        else {
+            System.out.println("Минимальное число " + a);
+        }
+    }
+}
