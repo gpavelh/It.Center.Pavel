8097a54ac5d122a351a655ed3f283ebc27ac87ea
Pavel <gh7777@yandex.ru>
2020-02-12 15:41:11 +0300

    Ex5

---
+++ src/Ex5.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Ex5 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String b = reader.readLine();
+        int a = Integer.parseInt(b);
+        if (a % 400 == 0)
+            System.out.println("Високосный год");
+        else if (a % 100 == 0)
+            System.out.println("Не високосный год");
+        else if (a % 4 == 0)
+            System.out.println("Високосный год");
+        else
+            System.out.println("Не високосный год");
+
+    }
+}
