2feaacd12e256a28dbc059e082f2466e7bf0a837
Pavel <gh7777@yandex.ru>
2020-02-13 15:03:40 +0300

    Lesson2Ex1 3.0 final

--- src/Lesson02/Lesson02Ex1.java
+++ src/Lesson02/Lesson02Ex1.java
@@ -8,12 +8,17 @@ import java.math.RoundingMode;
 
 public class Lesson02Ex1 {
     public static void main(String[] args) throws IOException {
-        Coca coca = new Coca("CocaLite",2.4, "a1");
-        Mars mars = new Mars("MarsWithNuts",2.1, "a2");
-        Milk milk = new Milk("Ostankino",1.2, "b1");
-        Snic snic = new Snic("Snickers",2.7, "b2");
+        Coca coca = new Coca("Кока-Кола Vanilla",2.4, "a1");
+        Mars mars = new Mars("Mars с Орехами",2.1, "a2");
+        Milk milk = new Milk("Домик в деревне",1.2, "b1");
+        Snic snic = new Snic("Snickers Super",2.7, "b2");
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Напиток:       " + coca.name +"     Цена:  " + coca.cost + "$"+ "     Позиция:  " + coca.position);
+        System.out.println("Шоколадка:     " + mars.name +"        Цена:  " + mars.cost + "$"+ "     Позиция:  " + mars.position);
+        System.out.println("Напиток:       " + milk.name +"       Цена:  " + milk.cost + "$"+ "     Позиция:  " + milk.position);
+        System.out.println("Шоколадка:     " + snic.name +"        Цена:  " + snic.cost + "$"+ "     Позиция:  " + snic.position);
+        System.out.println("");
         System.out.println("Внесите сумму в $: ");
         double a = Double.parseDouble(reader.readLine());
         System.out.println("Укажите позицию товара в формате \"Ряд\" и \"Номер\" (Пример: a2): ");
@@ -22,10 +27,10 @@ public class Lesson02Ex1 {
             if (a > coca.cost){
                 double q1 = a - coca.cost;
                 BigDecimal q = new BigDecimal(q1).setScale(2,RoundingMode.HALF_UP);
-                System.out.println("Спасибо за покупку! Заберите Вашу сачу: " + q + "$");
+                System.out.println("Спасибо за покупку  " +coca.name +"!"+" Заберите Вашу сачу: " + q + "$");
             }
             else if (a == coca.cost){
-                System.out.println("Спасибо за покупку!");
+                System.out.println("Спасибо за покупку" +coca.name +"!");
             }
             else if (a < coca.cost){
                 System.out.println("Недостаточно средств!");
@@ -38,10 +43,10 @@ public class Lesson02Ex1 {
             if (a > mars.cost){
                 double q1 = a - mars.cost;
                 BigDecimal q = new BigDecimal(q1).setScale(2,RoundingMode.HALF_UP);
-                System.out.println("Спасибо за покупку! Заберите Вашу сачу: " + q + "$");
+                System.out.println("Спасибо за покупку  " +mars.name +"!"+" Заберите Вашу сачу: " + q + "$");
             }
             else if (a == mars.cost){
-                System.out.println("Спасибо за покупку!");
+                System.out.println("Спасибо за покупку" +mars.name +"!");
             }
             else if (a < mars.cost){
                 System.out.println("Недостаточно средств!");
@@ -54,10 +59,10 @@ public class Lesson02Ex1 {
             if (a > milk.cost){
                 double q1 = a - milk.cost;
                 BigDecimal q = new BigDecimal(q1).setScale(2,RoundingMode.HALF_UP);
-                System.out.println("Спасибо за покупку! Заберите Вашу сачу: " + q + "$");
+                System.out.println("Спасибо за покупку  " +milk.name +"!"+" Заберите Вашу сачу: " + q + "$");
             }
             else if (a == milk.cost){
-                System.out.println("Спасибо за покупку!");
+                System.out.println("Спасибо за покупку" +milk.name +"!");
             }
             else if (a < milk.cost){
                 System.out.println("Недостаточно средств!");
@@ -70,10 +75,10 @@ public class Lesson02Ex1 {
             if (a > snic.cost){
                 double q1 = a - snic.cost;
                 BigDecimal q = new BigDecimal(q1).setScale(2,RoundingMode.HALF_UP);
-                System.out.println("Спасибо за покупку! Заберите Вашу сачу: " + q + "$");
+                System.out.println("Спасибо за покупку  " +mars.name +"!"+" Заберите Вашу сачу: " + q + "$");
             }
             else if (a == snic.cost){
-                System.out.println("Спасибо за покупку!");
+                System.out.println("Спасибо за покупку" +mars.name +"!");
             }
             else if (a < snic.cost){
                 System.out.println("Недостаточно средств!");
