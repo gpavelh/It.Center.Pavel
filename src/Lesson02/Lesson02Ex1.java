9822ad341063e64239be1ccd1864f4a73bdb3f54
Pavel <gh7777@yandex.ru>
2020-02-13 12:34:48 +0300

    Lesson2Ex1 1.0

--- src/Lesson02/Lesson02Ex1.java
+++ src/Lesson02/Lesson02Ex1.java
@@ -1,22 +1,67 @@
-package Lesson02;
+package Lesson02;//Авомат с вкусняшками
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Lesson02Ex1 {
-    private int top;
-    private int low;
+    public static void main(String[] args) throws IOException {
+        Coca coca = new Coca("CocaLite",2.4, "a1");
+        Mars mars = new Mars("MarsWithNuts",2.1, "a2");
+        Milk milk = new Milk("Ostankino",1.2, "b1");
+        Snic snic = new Snic("Snickers",2.1, "b2");
 
-    public int getTop() {
-        return top;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Внесите сумму в $: ");
+        int a = Integer.parseInt(reader.readLine());
+        System.out.println("Укажите позицию товара в формате \"Ряд\" и \"Номер\" (Пример: a2): ");
+        String y = reader.readLine();
+        if (y == coca.position){
+            if ()
+        }
     }
+    public static class Coca{
+        String name;
+        Double cost;
+        String position;
 
-    public void setTop(int top) {
-        this.top = top;
+        public Coca(String name, double cost, String position){
+            this.name = name;
+            this.cost = cost;
+            this.position = position;
+        }
     }
+    public static class Mars {
+        String name;
+        double cost;
+        String position;
 
-    public int getLow() {
-        return low;
+        public Mars(String name, double cost, String position){
+            this.name = name;
+            this.cost = cost;
+            this.position = position;
+        }
     }
+    public static class Milk {
+        String name;
+        double cost;
+        String position;
 
-    public void setLow(int low) {
-        this.low = low;
+        public Milk(String name, double cost, String position){
+            this.name = name;
+            this.cost = cost;
+            this.position = position;
+        }
+    }
+    public static class Snic {
+        String name;
+        double cost;
+        String position;
+
+        public Snic (String name, double cost, String position){
+            this.name = name;
+            this.cost = cost;
+            this.position = position;
+        }
     }
 }
