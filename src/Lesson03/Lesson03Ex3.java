f3dc95ed713cb3a3dbbfddc61b5c97103cdbfdba
Pavel <gh7777@yandex.ru>
2020-02-14 16:38:44 +0300

    Lesson3Ex3 final

---
+++ src/Lesson03/Lesson03Ex3.java
@@ -0,0 +1,25 @@
+package Lesson03;//Ряд Фибонначчи
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Lesson03Ex3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        System.out.println(fiban(a));
+    }
+    public static int fiban(int a){
+        if (a == 0)
+        {
+            return 0;
+        }
+        if (a == 1)
+        {
+            return 1;
+        }
+        return fiban(a - 1)  + fiban(a - 2);
+    }
+}
