5f790b1fdcf7d53847ac00d1d86aa29b5e699a94
gh <gh7777@mail.ru>
2020-02-16 21:49:59 +0300

    Lesson04Ex3 final

---
+++ src/Lesson04/Lesson04Ex3.java
@@ -0,0 +1,93 @@
+package Lesson04;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Lesson04Ex3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите первое число: ");
+        double a = Double.parseDouble(reader.readLine());
+        System.out.println("Введите второе число: ");
+        double b = Double.parseDouble(reader.readLine());
+        System.out.println("Выберите тип операции:\n 1 - \"Вычитание\"  2 - \"Сложение\"  3 - \"Умножение\"  4 - \"Деление\"  5 - \"Процент от числа\"");
+        int c = Integer.parseInt(reader.readLine());
+        if (c == 1){
+            Calculator.addition(a, b);
+        }
+        else if (c == 2){
+            Calculator.subtraction(a, b);
+        }
+        else if (c == 3){
+            Calculator.multiplication(a,b);
+        }
+        else if (c == 4){
+            Calculator.division(a,b);
+        }
+        else if (c == 5) {
+            Calculator.percent(a, b);
+        }
+        else
+            System.out.println("Ошибка ввода");
+    }
+    public static class Calculator {
+        public static void addition(double a, double b) {
+            double ans = a + b;
+            if (ans % 1 == 0)
+            {
+                System.out.println("Ответ: " + (int) (ans));
+            }
+            else {
+            BigDecimal ans1 = new BigDecimal(ans).setScale(1, RoundingMode.HALF_UP);
+            System.out.println("Ответ: " + ans1);
+            }
+        }
+        public static void subtraction(double a, double b) {
+            double ans = a - b;
+            if (ans % 1 == 0)
+            {
+                System.out.println("Ответ: " + (int) (ans));
+            }
+            else {
+                BigDecimal ans1 = new BigDecimal(ans).setScale(1, RoundingMode.HALF_UP);
+                System.out.println("Ответ: " + ans1);
+            }
+        }
+        public static void multiplication(double a, double b) {
+            double ans = a * b;
+            if (ans % 1 == 0)
+            {
+                System.out.println("Ответ: " + (int) (ans));
+            }
+            else {
+                BigDecimal ans1 = new BigDecimal(ans).setScale(1, RoundingMode.HALF_UP);
+                System.out.println("Ответ: " + ans1);
+            }
+        }
+        public static void division(double a, double b) {
+            double ans = a / b;
+            if (ans % 1 == 0)
+            {
+                System.out.println("Ответ: " + (int) (ans));
+            }
+            else {
+                BigDecimal ans1 = new BigDecimal(ans).setScale(1, RoundingMode.HALF_UP);
+                System.out.println("Ответ: " + ans1);
+            }
+        }
+        public static void percent(double a, double b){
+            double ans = b * (a/100);
+            if (ans % 1 == 0)
+            {
+                System.out.println("Ответ: " + (int) (ans));
+            }
+            else {
+                BigDecimal ans1 = new BigDecimal(ans).setScale(1, RoundingMode.HALF_UP);
+                System.out.println("Ответ: " + ans1);
+            }
+        }
+
+    }
+}
