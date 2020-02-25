1d0ee4ec32ffb45ae6ae4d705b897948ebacbba0
Pavel <gh7777@yandex.ru>
2020-02-25 18:31:23 +0300

    Lesson6Ex4 final

---
+++ src/Lesson06/Lesson06Ex4.java
@@ -0,0 +1,31 @@
+package Lesson06;
+
+import java.util.*;
+
+public class Lesson06Ex4 {
+    public Set<String> removeEvenLenght(Set<String> set){
+        Set<String> finish = new HashSet<>();
+        for (String s: set
+             ) {
+            if (s.length() % 2 != 0) {
+                finish.add(s);
+            }
+        }
+        return finish;
+    }
+
+    public static void main(String[] args) {
+        Set<String> words = new HashSet<>();
+        words.add("foo");
+        words.add("buzz");
+        words.add("bar");
+        words.add("fork");
+        words.add("bort");
+        words.add("spoon");
+        words.add("!");
+        words.add("dude");
+        words.add("dud1e");
+        Lesson06Ex4 removeValues = new Lesson06Ex4();
+        System.out.println(removeValues.removeEvenLenght(words));
+    }
+}
